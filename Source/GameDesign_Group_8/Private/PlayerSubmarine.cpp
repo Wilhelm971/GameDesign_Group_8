10dcda403908ae8b7c6be54943648ec41b5dbd07
Wilhelm971 <276351@stud.inn.no>
2025-04-06 19:00:55 +0200

    Pawn
    Made the pawn in c++

---
+++ Source/GameDesign_Group_8/Private/PlayerSubmarine.cpp
@@ -0,0 +1,181 @@
+// Copyright W.V. Koren. All Rights Reserved
+
+
+#include "PlayerSubmarine.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Physics/ImmediatePhysics/ImmediatePhysicsShared/ImmediatePhysicsCore.h"
+
+
+// Sets default values
+APlayerSubmarine::APlayerSubmarine()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	Box -> SetBoxExtent(FVector(32.0f, 32.0f, 32.0f));
+
+	Box -> SetCollisionProfileName(TEXT("Pawn"));
+
+	SubmarineMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SubmarineMesh"));
+	SubmarineMesh -> SetupAttachment(RootComponent);
+	SubmarineMesh -> SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	RootComponent = SubmarineMesh;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm -> SetupAttachment(RootComponent);
+	SpringArm -> bUsePawnControlRotation = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera -> SetupAttachment(SpringArm);
+
+	bUseControllerRotationYaw = false;
+
+	MoveForce = 30000.0f;
+	TorqueForce = 200000.0f;
+	ElevateForce = 15000.0f;
+
+}
+
+// Called when the game starts or when spawned
+void APlayerSubmarine::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerSubmarine::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//StraightenBoatLevel();
+
+}
+
+// Called to bind functionality to input
+void APlayerSubmarine::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(InputMappingContext, 0);
+		}
+
+	}
+	if (UEnhancedInputComponent* Input = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		Input->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerSubmarine::Move);
+		Input->BindAction(RotateAction, ETriggerEvent::Triggered, this, &APlayerSubmarine::Rotate);
+		Input->BindAction(ElevateAction, ETriggerEvent::Triggered, this, &APlayerSubmarine::Elevate);
+
+		Input->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerSubmarine::Look);
+		
+		Input->BindAction(PauseAction, ETriggerEvent::Started, this, &APlayerSubmarine::Pause);
+		Input->BindAction(InteractAction, ETriggerEvent::Triggered, this, &APlayerSubmarine::InteractWithObject);
+	}
+
+}
+
+
+
+void APlayerSubmarine::Move_Implementation(const FInputActionValue& InputValue)
+{
+	FVector2d InputVector = InputValue.Get<FVector2D>();
+
+	if (IsValid(Controller))
+	{
+
+		FVector ForwardsForce = InputVector.Y * MoveForce * SubmarineMesh -> GetForwardVector();
+		
+		
+		if (InputVector.Y < 0)
+		{
+			FVector BackwardsForce = ForwardsForce * 0.5f;
+			SubmarineMesh->AddForce(BackwardsForce);
+			
+			
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Moving Backwards")));
+		}
+
+		else
+		{
+			SubmarineMesh->AddForce(ForwardsForce);
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Moving Forwards")));
+	
+		}
+	}
+	
+}
+
+void APlayerSubmarine::Rotate_Implementation(const FInputActionValue& InputValue)
+{
+
+	FVector InputVector = InputValue.Get<FVector>();
+	if (IsValid(Controller))
+	{
+		SubmarineMesh->AddTorqueInRadians(SubmarineMesh->GetUpVector() * TorqueForce * InputVector);
+
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Rotating")));
+	}
+}
+
+void APlayerSubmarine::Elevate_Implementation(const FInputActionValue& InputValue)
+{
+
+	FVector2d InputVector = InputValue.Get<FVector2D>();
+
+	if (IsValid(Controller))
+	{
+		SubmarineMesh->AddForce(ElevateForce * InputVector.Y * GetActorUpVector());
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Elevating")));
+	}
+	
+
+}
+
+void APlayerSubmarine::Look_Implementation(const FInputActionValue& InputValue)
+{
+	FVector2D InputVector = InputValue.Get<FVector2D>();
+
+	if (IsValid(Controller))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Looking")));
+		AddControllerYawInput(InputVector.X);
+		AddControllerPitchInput(InputVector.Y);
+	}
+}
+
+void APlayerSubmarine::Pause()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Pause")));
+}
+
+void APlayerSubmarine::StraightenBoatLevel()
+{
+/*
+	if (SubmarineMesh->GetRelativeRotation().Yaw > 0)
+	{
+		
+		SubmarineMesh -> SetRelativeRotation()
+	}
+	*/
+}
+
+
+void APlayerSubmarine::InteractWithObject()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Interact")));
+}
---
+++ Source/GameDesign_Group_8/Public/PlayerSubmarine.h
@@ -0,0 +1,100 @@
+// Copyright W.V. Koren. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+
+#include "InputActionValue.h"
+
+#include "PlayerSubmarine.generated.h"
+
+UCLASS()
+class GAMEDESIGN_GROUP_8_API APlayerSubmarine : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* Camera;
+
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* SpringArm;
+	
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* Box;
+
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* SubmarineMesh;
+
+	
+	// Sets default values for this pawn's properties
+	APlayerSubmarine();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+// Enhanced Input System
+	protected:
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* RotateAction;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* ElevateAction;
+	
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* PauseAction;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* InteractAction;
+
+
+
+	// Movement Functions
+protected:
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Movement")
+	void Move(const FInputActionValue& InputValue);
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Movement")
+	void Rotate(const FInputActionValue& InputValue);
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Movement")
+	void Elevate(const FInputActionValue& InputValue);
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Movement")
+	void Look(const FInputActionValue& InputValue);
+
+	
+	
+
+	void InteractWithObject();
+
+	void Pause();
+
+
+	float MoveForce;
+	float TorqueForce;
+	float ElevateForce;
+
+	void StraightenBoatLevel();
+	
+};
