0feef549a8eb5bd99b60598683c9e4e9714ea980
martinsolvang <54808703+martinsolvang@users.noreply.github.com>
2025-05-05 14:06:47 +0200

    hmm

--- Source/GameDesign_Group_8/Private/Boid/CPP_BaseFishAI.cpp
+++ Source/GameDesign_Group_8/Private/Boid/CPP_BaseFishAI.cpp
@@ -1,8 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Components/SplineComponent.h"
 #include "Boid/CPP_BaseFishAI.h"
-
+#include "Components/SplineComponent.h"
 
 ACPP_BaseFishAI::ACPP_BaseFishAI()
 {
--- Source/GameDesign_Group_8/Public/TestLevelGamemode.h
+++ Source/GameDesign_Group_8/Public/TestLevelGamemode.h
@@ -3,19 +3,18 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "FunctionalTestGameMode.h"
+#include "GameFramework/GameModeBase.h"
 #include "TestLevelGamemode.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class GAMEDESIGN_GROUP_8_API ATestLevelGamemode : public AFunctionalTestGameMode
+class GAMEDESIGN_GROUP_8_API ATestLevelGamemode : public AGameModeBase 
 {
 	GENERATED_BODY()
 
 protected:
 	virtual void BeginPlay() override;
-
-
+	
 };
